30541e93f81a618918cba9869be4686c46794d83
ishleigh <71249015+ishleigh@users.noreply.github.com>
2021-06-14 11:34:15 +0900

    Add files via upload

---
+++ input.py
@@ -0,0 +1,12 @@
+# input a float value for variable a here
+a=float(input("First number is:"))
+# input a float value for variable b here
+b=float(input("Second number is:"))
+# output the result of addition here
+print("Addition result is: " +str(a+b))
+# output the result of subtraction here
+print("Subtraction result is: " +str(a-b))
+# output the result of multiplication here
+# output the result of division here
+
+print("\nThat's all, folks!")
